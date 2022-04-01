4225b2bc69084a06d188a664cbc4ff9ed4c927ae
Сокова Анастасия <99127518+AnastasiaSokova@users.noreply.github.com>
2022-04-01 13:24:26 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,29 @@
 
 template<typename T, int size>
 class TStack {
-
+private:
+    T arr[100];
+    int top;
+public:
+    TStack(): top(-1) { }
+    bool isEmpty() {
+        return top == -1;
+    }
+    bool isFull() {
+        return top == size - 1;
+    }
+    void pop() {
+        if (!(isEmpty()))
+            top--;
+    }
+    void push(T value) {
+        if (isFull())
+            throw "Full !";
+        else
+            arr[++top] = value;
+    }
+    T get() {
+        return arr[top];
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
